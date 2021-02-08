e17ff4fb59ca8ccc86789314556998dfbed0cb13
Jsamapro <78345756+Jsamapro@users.noreply.github.com>
2021-02-08 10:11:49 +0100

    Update snippet_kill_switch.cpp

--- snippet_kill_switch.cpp
+++ snippet_kill_switch.cpp
@@ -2,8 +2,35 @@
 
 int switchState = 0;
 bool isTheButtonBeingPressed = false;
-bool pinPinflag = false;
+bool play = false;
 
 //recordar que el pin del botón puede cambiar
-
 const int buttonPin=2;
+
+//incluir dentro del setup()
+
+pinMode(pinButton,IMPUT)
+//incluir como loop()
+  
+void loop() {
+  checkButton();
+  if (play) {
+   //
+  }
+}
+
+// funcion del checkButton
+
+
+void checkButton(){
+  switchState = digitalRead(buttonPin);
+  if (switchState == HIGH){
+    if (isTheButtonBeingPressed == false){
+      play = !play;
+      isTheButtonBeingPressed = true;
+  }
+}
+else{
+  isTheButtonBeingPressed = false;
+}
+}
