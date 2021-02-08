91df332a85d458b63fa6c7e0c5cb17f9e5a43063
Jsamapro <78345756+Jsamapro@users.noreply.github.com>
2021-02-08 09:49:13 +0100

    Create snippet_kill_switch.cpp

---
+++ snippet_kill_switch.cpp
@@ -0,0 +1,9 @@
+//colocar en la seccion de variables
+
+int switchState = 0;
+bool isTheButtonBeingPressed = false;
+bool pinPinflag = false;
+
+//recordar que el pin del botón puede cambiar
+
+const int buttonPin=2;
